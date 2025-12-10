fdfda91c6bbc0f66c209f82999e676df852c07fe
IvanMatiasBer <kiramclashr@gmail.com>
2025-12-10 16:23:34 +0100

    sepso3

--- Assets/Scripts/Contador.cs
+++ Assets/Scripts/Contador.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Contador : MonoBehaviour
+public class NivelTimerBarraColor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Configuración")]
+    public float tiempoInicial = 30f;
+
+    [Header("Referencias UI")]
+    public Slider barraTiempo;
+    public Image fillBar;               // La parte de color de la barra
+    public GameObject cartelDiaTerminado;
+
+    private float tiempoRestante;
+    private bool nivelIniciado = false;
+
     void Start()
     {
-        
+        tiempoRestante = tiempoInicial;
+
+        // Configurar barra
+        barraTiempo.maxValue = tiempoInicial;
+        barraTiempo.value = tiempoInicial;
+
+        cartelDiaTerminado.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (nivelIniciado && tiempoRestante > 0)
+        {
+            tiempoRestante -= Time.deltaTime;
+
+            if (tiempoRestante <= 0)
+            {
+                tiempoRestante = 0;
+                TerminarDia();
+            }
+
+            // Actualizar barra
+            barraTiempo.value = tiempoRestante;
+
+            // Cambiar color según tiempo restante
+            ActualizarColorBarra();
+        }
+    }
+
+    public void EmpezarNivel()
+    {
+        nivelIniciado = true;
+    }
+
+    void TerminarDia()
+    {
+        nivelIniciado = false;
+        cartelDiaTerminado.SetActive(true);
+    }
+
+    void ActualizarColorBarra()
+    {
+        float porcentaje = tiempoRestante / tiempoInicial;
+
+        if (porcentaje > 0.5f)
+        {
+            fillBar.color = Color.green;
+        }
+        else if (porcentaje > 0.25f)
+        {
+            fillBar.color = Color.yellow;
+        }
+        else
+        {
+            fillBar.color = Color.red;
+        }
     }
 }
