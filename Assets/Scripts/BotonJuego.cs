f8178366017725f0cafb7ef5daa9adca303260e6
ncarrasb <carrascosa.brotons06@gmail.com>
2025-12-12 20:10:16 +0100

    Botones funcionando suuuuuuuuuu

--- Assets/Scripts/BotonJuego.cs
+++ Assets/Scripts/BotonJuego.cs
@@ -14,22 +14,23 @@ public class BotonJuego : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerManager = FindFirstObjectByType<PlayerManager>();
-
+        /*
         if (spriteRenderer && ingrediente.sprite)
             spriteRenderer.sprite = ingrediente.sprite;
+        */
     }
 
     void OnMouseDown()
     {
         transform.localScale = Vector3.one * 0.9f;
 
-        if (ingrediente.tipo == "recipiente")
+        if (ingrediente.tipo == "Recipiente")
             playerManager.ElegirRecipiente(ingrediente);
-        else if (ingrediente.tipo == "helado")
+        else if (ingrediente.tipo == "Helado")
             playerManager.ElegirHelado(ingrediente);
-        else if (ingrediente.tipo == "sabor")
+        else if (ingrediente.tipo == "Sabor")
             playerManager.ElegirSabor(ingrediente);
-        else if (ingrediente.tipo == "topping")
+        else if (ingrediente.tipo == "Topping")
             playerManager.ElegirTopping(ingrediente);
     }
 
