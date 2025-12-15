0b2a172933a36e3c91639c3848f48cb872838d69
Nicoam706 <thenicoam@gmail.com>
2025-12-15 12:59:25 +0100

    Cambios varios y ajuste de sprites

--- Assets/Scripts/BotonJuego.cs
+++ Assets/Scripts/BotonJuego.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class BotonJuego : MonoBehaviour
 {
-    [Header("Configuración del Botón")]//sexooo
+    [Header("Configuración del Botón")]
     public Ingredientes ingrediente;
     public PlayerManager playerManager;
 
